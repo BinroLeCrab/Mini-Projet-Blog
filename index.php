29faec141ba69094746e61d87f6fa65bd785d7e9
Robin Vigier / B!nro <rob.vigier@gmail.com>
2023-11-14 09:54:42 +0100

    :tada: Début du Mini Projet

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Mini Projet Blog</title>
+        <link rel="stylesheet" type="text/css" href="style/style.css"/>
+    </head>
+    <body>
+        <h1>Le blog ! </h1>
+    </body>
+</html>
+
+<!-- 
+     _____     _____
+    |  _  |   |  _  |
+   -| | | |---| | | |-
+    |_____| 7 |_____|  ~B!nro~
+    
+-->
